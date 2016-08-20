285d0588811981e415064abd4c7291c16fb3d495
congcong_ai <congcong_ai@kingdee.com>
2016-08-20 09:58:19 +0800

    update

--- bdmLib/recommender/CBBasedRecs.py
+++ bdmLib/recommender/CBBasedRecs.py
@@ -6,31 +6,6 @@ from ..util import constants
 from ..models.word import Word
 import pubUtil
 
-def TFIDFBasedRecs_old(message_lst,num_features=1000,sim=constants.COS,top = 10):
-    """
-      content(tf/idf)-based recommendation algorithm.
-
-      Parameters
-      ------------
-       `message_lst` —— a list who's element is a post(aritle,etc)  
-      `num_features`——length of the Feature vector 
-      `sim`—— the similarity algorithm
-      'top'——the length of the recommendation list
-      `message`:a post that has not split.
-      `comment`:the post that has split to word
-    """
-    comment_lst = []
-    for message in message_lst:
-        comment = file_process.split_comments(message)
-        #print json.dumps(comment, encoding="UTF-8", ensure_ascii=False)
-        comment_lst.append(comment)
-    tf_idf_matrix = TFIDF_matrix(comment_lst,num_features)
-   # print tf_idf_matrix
-    #sys.exit(-1)
-    rating_map = pubUtil.count_similarity_based_matrix(tf_idf_matrix,tf_idf_matrix,sim)
-    recommendation_lsts = pubUtil.return_init_recommendations(rating_map,top)
-    print rating_map
-    print recommendation_lsts
 
 def TFIDFBasedRecs(source_matrix,be_recommend_matrix=None,sim=constants.PEARSON,threshold = None,top = 10,keep_sim = False):
     """
@@ -53,8 +28,8 @@ def TFIDFBasedRecs(source_matrix,be_recommend_matrix=None,sim=constants.PEARSON,
     start1 = time.clock()
     recommendation_lsts = pubUtil.get_recommendation_list(source_matrix,be_recommend_matrix,sim,top,threshold,keep_sim)
     start2 = time.clock()
-    print '获取推荐列表耗时: ',str(start2-start1)
-    print recommendation_lsts
+    #print '获取推荐列表耗时: ',str(start2-start1)
+    return  recommendation_lsts
 def TFIDF_matrix(message_lst,num_features=1000):
     """
      a matrix who's element is word‘s tf/idf.
@@ -69,27 +44,27 @@ def TFIDF_matrix(message_lst,num_features=1000):
         #print json.dumps(comment, encoding="UTF-8", ensure_ascii=False)
         comment_lst.append(comment)
     start2 = time.clock()
-    print '分词耗时: ',str(start2-start1)
+    #print '分词耗时: ',str(start2-start1)
     matrix = []
     #the number of posts
     num_docs = len(comment_lst)
     start1 = time.clock()
     top_words = file_process.find_topwords(comment_lst,num_features)
     start2 = time.clock()
-    print '找特征词耗时: ',str(start2-start1)
+    #print '找特征词耗时: ',str(start2-start1)
     start1 = time.clock()
     for word in top_words:
         #print word.name,':',word.freq,':',word.doc_freq
         word.doc_freq = tf_idf.find_word_doc_freq(word.name,comment_lst)
     start2 = time.clock()
-    print '计算词的doc freq耗时: ',str(start2-start1)
-    print '开始求特征矩阵... ...'
+    #print '计算词的doc freq耗时: ',str(start2-start1)
+    #print '开始求特征矩阵... ...'
     start1 = time.clock()
     for comment in comment_lst:
        vector = tf_idf.TFIDF_vector(comment,num_docs,top_words)
        matrix.append(vector)
     start2 = time.clock()
-    print '得到tf/idf特征矩阵耗时: ',str(start2-start1)
+    #print '得到tf/idf特征矩阵耗时: ',str(start2-start1)
     return matrix
 
 
