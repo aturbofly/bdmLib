ad46c7880cf47659c415e4dbd70467b19286f974
congcong_ai <congcong_ai@kingdee.com>
2016-08-20 09:54:10 +0800

    delete build

--- build/lib/ssjrecsys/models/class.py
+++
@@ -1,6 +0,0 @@
-
-# class file(object):
-#     """docstring for file"""
-#     def __init__(self, arg):
-#         self.arg = arg
-#     def get_
--- build/lib/ssjrecsys/models/word.py
+++
@@ -1,8 +0,0 @@
-
-class Word(object):
-    """"""
-    def __init__(self, name,freq=0,doc_freq=0):
-        self.name = name
-        self.freq = freq
-        self.doc_freq = doc_freq
-        
